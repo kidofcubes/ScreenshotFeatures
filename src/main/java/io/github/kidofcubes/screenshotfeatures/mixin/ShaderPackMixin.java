d940fced43e1a360aac9a33ce32a72233e3cd525
kidofcubes <kidofcubes@gmail.com>
2026-08-08 04:38:42 +0800

    quicksave7

--- src/main/java/io/github/kidofcubes/screenshotfeatures/mixin/ShaderPackMixin.java
+++ src/main/java/io/github/kidofcubes/screenshotfeatures/mixin/ShaderPackMixin.java
@@ -17,21 +17,74 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
 @Mixin(value = ShaderPack.class, remap = false)
 public class ShaderPackMixin {
-    @Inject(method="lambda$new$8", at=@At(value="INVOKE", target="Lnet/irisshaders/iris/shaderpack/preprocessor/JcppProcessor;glslPreprocessSource(Ljava/lang/String;Ljava/lang/Iterable;)Ljava/lang/String;"))
-    private static void injectUniforms(CallbackInfoReturnable<String> cir,@Local(name="source") LocalRef<String> source, @Local(name="finalEnvironmentDefines1") LocalRef<Iterable<StringPair>> finalEnvironmentDefines1){
+//    @Inject(method="lambda$new$8", at=@At(value="INVOKE", target="Lnet/irisshaders/iris/shaderpack/preprocessor/JcppProcessor;glslPreprocessSource(Ljava/lang/String;Ljava/lang/Iterable;)Ljava/lang/String;"))
+    @ModifyArgs(method="lambda$new$8", at=@At(value="INVOKE", target="Lnet/irisshaders/iris/shaderpack/preprocessor/JcppProcessor;glslPreprocessSource(Ljava/lang/String;Ljava/lang/Iterable;)Ljava/lang/String;"))
+//    private static void injectUniforms(CallbackInfoReturnable<String> cir,@Local(name="source") LocalRef<String> source, @Local(name="finalEnvironmentDefines1") LocalRef<Iterable<StringPair>> finalEnvironmentDefines1){
+    private static void injectUniforms(Args args){
 //        source doesn't have #defines already now
         // just need to try to fix anything that was using the defined variable
         // then add the uniform to the top
+
+        List<String> overridedUniforms = new ArrayList<>();
+        for(var entry: Configs.CustomUniforms.CUSTOM_UNIFORMS.getEntries()){
+            if(entry.override){
+                overridedUniforms.add(entry.getName());
+            }
+        }
+
+
+        var iterable = (Iterable<StringPair>)args.get(1);
+        Iterable<StringPair> filtered = Streams.stream(iterable).filter(pair -> {
+            return !overridedUniforms.contains(pair.key());
+        })::iterator;
+        args.set(1,filtered);
+
+
+        String source = args.get(0);
+
+        for(String overridedUniform: overridedUniforms){
+            String regex = "(?m)^(\\s*#define\\s+)" + Pattern.quote(overridedUniform) + "(\\s+)[+-]?\\d*(?:\\.\\d+)?(\\s*//.*)?$";
+            source = source.replaceAll(regex, "");
+        }
+
+
+
+        StringBuilder builder = new StringBuilder();
+//        for(int i=0;i<tokenList.size();i++){
+//            builder.append(tokenList.get(i).getText());
+//        }
+//        builder.append((String)args.get(0));
+        builder.append((String)source);
+        StringBuilder uniformsBuilder = new StringBuilder();
+        for(String overridedUniform: overridedUniforms){
+            uniformsBuilder.append("uniform float " + overridedUniform + ";\n");
+        }
+        String origBuilder = builder.toString();
+        builder.insert( builder.indexOf("\n", builder.indexOf("#version"))+1,uniformsBuilder);
+        args.set(0,builder.toString());
+
+//        source.set(builder.toString());
+//
+
+//        builder.append(tok.getText());
+    }
+
+    @Inject(method="lambda$new$8", at=@At(value="RETURN", ordinal = 2), cancellable = true)
+    private static void unConst(CallbackInfoReturnable<String> cir){
 
         List<String> overridedUniforms = new ArrayList<>();
         for(var entry: Configs.CustomUniforms.CUSTOM_UNIFORMS.getEntries()){
@@ -52,7 +105,7 @@ public class ShaderPackMixin {
 
         GlslCollectingListener listener = new GlslCollectingListener();
         pp.setListener(listener);
-        pp.addInput(new StringLexerSource(source.get(), true));
+        pp.addInput(new StringLexerSource(cir.getReturnValue(), true));
         pp.addFeature(Feature.KEEPCOMMENTS);
 
         List<Token> tokenList = new ArrayList<>();
@@ -74,7 +127,7 @@ public class ShaderPackMixin {
             (x -> x.getText().equals("float")),
             (x -> x.getType()==Token.IDENTIFIER),
             (x -> x.getText().equals("=")), //61
-            (x -> x.getType()==Token.NUMBER)
+            (x -> x.getText().equals(";"))
         );
 
         List<Predicate<Token>> defineMatch = List.of(
@@ -85,43 +138,29 @@ public class ShaderPackMixin {
             (x -> x.getType()==Token.CPPCOMMENT)
         );
         int searchingIndex = 0;
-        while(searchingIndex<0){
+        while(true){
             List<Integer> matchedConst = matchIndex(tokenList, searchingIndex, x -> x.getType() == Token.WHITESPACE||x.getText().equals("-"), constMatch);
             if(matchedConst.getFirst()==-1){
                 break;
             }
 
-            if(overridedUniforms.contains(tokenList.get(matchedConst.get(2)).getText())){ // we unconst it
-//                System.out.println("found a const of"  +
-//                    "  '"+tokenList.get(matchedConst.get(0)).getText()+
-//                    "' '"+tokenList.get(matchedConst.get(1)).getText()+
-//                    "' '"+tokenList.get(matchedConst.get(2)).getText()+
-//                    "' '"+tokenList.get(matchedConst.get(3)).getText()+
-//                    "' '"+tokenList.get(matchedConst.get(4)).getText()+
-//                    "'  ");
-                System.out.println("found");
-                for(int i=matchedConst.get(0);i<=matchedConst.get(4);i++){
-                    System.out.print(tokenList.get(i).getText());
+            boolean unconst = false;
+            for(int i=matchedConst.get(3)+1;i<matchedConst.get(4);i++){
+                if(overridedUniforms.contains(tokenList.get(i).getText())){
+                    unconst = true;
+                    break;
                 }
-//                var orig0 = tokenList.get(matchedConst.get(0));
-//                tokenList.set(matchedConst.get(0), new Token(Token.WHITESPACE, orig0.getLine(), orig0.getColumn(), "     ", null));
-                tokenList.set(matchedConst.get(0), new Token(Token.IDENTIFIER, -1, -1, "uniform", null));
-                tokenList.set(matchedConst.get(3), new Token(Token.WHITESPACE, -1, -1, " ", null));
-                for(int i=matchedConst.get(3)+1;i<=matchedConst.get(4);i++){
-                    tokenList.set(i, new Token(Token.WHITESPACE, -1, -1, " ", null));
-                }
-                System.out.println("replaced into");
-                for(int i=matchedConst.get(0);i<=matchedConst.get(4);i++){
-                    System.out.print(tokenList.get(i).getText());
-                }
-                System.out.println("");
+            }
+
+            if(unconst){
+                //todo remove the const definition, put it in parentheses, and replace usages of it everywhere else
+//                tokenList.set(matchedConst.get(0), new Token(Token.WHITESPACE, -1, -1, "uniform", null));
+
             }
 
             searchingIndex = matchedConst.getFirst() + 1;
         }
-        finalEnvironmentDefines1.set(Streams.stream(finalEnvironmentDefines1.get()).filter(pair -> {
-            return !overridedUniforms.contains(pair.key());
-        })::iterator);
+
 
 //        while(true){
 //            List<Integer> matchedDefine = matchIndex(tokenList,searchingIndex,x -> x.getType()==Token.WHITESPACE||x.getText().equals("-"),constMatch);
@@ -145,24 +184,10 @@ public class ShaderPackMixin {
 //            }
 //        }
 
-        StringBuilder builder = new StringBuilder();
-        for(int i=0;i<tokenList.size();i++){
-            builder.append(tokenList.get(i).getText());
-        }
-        for(String overridedUniform: overridedUniforms){
-            System.out.println("WE ADDED UNIFORM "+overridedUniform);
-            System.out.println("WE ADDED UNIFORM "+overridedUniform);
-            System.out.println("WE ADDED UNIFORM "+overridedUniform);
-            System.out.println("WE ADDED UNIFORM "+overridedUniform);
-            System.out.println("WE ADDED UNIFORM "+overridedUniform);
-            builder.insert(0, "uniform float " + overridedUniform + ";\n");
-            System.out.println("WE ADDED UNIFORM "+overridedUniform);
-        }
-        source.set(builder.toString());
-//
-
-//        builder.append(tok.getText());
     }
+
+
+
     @Unique
     private static <T> List<Integer> matchIndex(List<T> list,int startingIndex,Predicate<T> ignore,List<Predicate<T>> match){
         List<Integer> indexes = new ArrayList<>(Collections.nCopies(match.size(),0));
